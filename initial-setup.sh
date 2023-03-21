63365057b09772911d19e9ccbc3ad8f88ca04344
Markus Sachs <markus.sachs@ametras.com>
2023-03-21 13:54:27 +0100

    add passwd for admins

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Abfrage der Werte
 echo
 echo
 echo Grundkonfig Oddo Appliance
@@ -10,6 +11,15 @@ read -p"IP_Address (x.x.x.x/24): " IP_Address
 read -p"GW-IP: " IP_Gateway
 read -p"DNS-IP: " IP_DNS_1
 read -p"NTP-Server-IP: " NTPServer
+
+#Generieren Passwörter für CBL/BI-Admin User
+
+pwd_cbladmin = $(echo $RANDOM | md5sum | head -c 20)
+pwd_biadmin = $(echo $RANDOM | md5sum | head -c 20)
+
+echo $pwd_cbladmin
+echo $pwd_biadmin
+
 ansible-playbook -c local -e hostname_fqdn=$hostname_fqdn -e IP_Address=$IP_Address -e IP_Gateway=$IP_Gateway -e IP_DNS_1=$IP_DNS_1 -e NTPServer=$NTPServer /home/si/initial-setup/local.yml
 
 mv ~/.bash_login ~/.bash_login.done
