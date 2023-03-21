07c5f2c33e185f0762dca521807560c42ad2154b
Markus Sachs <markus.sachs@ametras.com>
2023-03-21 13:45:35 +0100

    insert reboot after config machine

--- initial-setup.sh
+++ initial-setup.sh
@@ -13,3 +13,11 @@ read -p"NTP-Server-IP: " NTPServer
 ansible-playbook -c local -e hostname_fqdn=$hostname_fqdn -e IP_Address=$IP_Address -e IP_Gateway=$IP_Gateway -e IP_DNS_1=$IP_DNS_1 -e NTPServer=$NTPServer ./local.yml
 
 mv ~/.bash_login ~/.bash_login.done
+
+echo
+echo
+read -p"Alles erledigt VM rebooten? (j/n)? " response
+
+    if [ "$response" == "j" ]; then
+        reboot
+    fi
