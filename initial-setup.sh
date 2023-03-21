3e2f675dafc0575480dc11c8e373e46bdf840c8d
Markus Sachs <markus.sachs@ametras.com>
2023-03-21 13:37:47 +0100

    fix Typo

--- initial-setup.sh
+++ initial-setup.sh
@@ -7,5 +7,5 @@ read -p"Hostname FQDN:  " hostname_fqdn
 read -p"IP_Address (x.x.x.x/24): " IP_Address
 read -p"GW-IP: " IP_Gateway
 read -p"DNS-IP: " IP_DNS_1
-read -p"NTP-Server-IP" NTPServer
+read -p"NTP-Server-IP: " NTPServer
 ansible-playbook -c local -e hostname_fqdn=$hostname_fqdn -e IP_Address=$IP_Address -e IP_Gateway=$IP_Gateway -e IP_DNS_1=$IP_DNS_1 -e NTPServer=$NTPServer ./local.yml
